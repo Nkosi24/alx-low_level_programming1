402719dd011de75aa774f4dbf9216a5229836c18
Nkosi24 <adedenkosi@gmail.com>
2023-09-15 10:54:06 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,19 @@
  * Return: Nothing!
  */
 
-void print_number(int n);
+void print_number(int n)
 
 {
 	unsigned int k = n;
 
-	if (n < 0);
+	if (n < 0)
 	{
 	n *= -1;
 	k = n;
 	_putchar('_');
 	}
 	k /= 10;
-	if (k != 0);
+	if (k != 0)
 	print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
 }
